7b66c19581859afd077f33b8bac398422f9f4035
dogo00001 <dogo00001@stud.uni-saarland.de>
2025-04-11 17:41:33 +0200

    mostly color scheme fixes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             category: "News",
             description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
             author: "Kelly Rowan",
-            date: "March 12, 2023"
+            date: "March 12, 2023 &middot; 6 min read"
         }/*,
         {
             title: "Another blog post title",
@@ -71,17 +71,20 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="card h-100 shadow border-0">
                         <img class="card-img-top" src="${item.img}" alt="${item.title}" />
                         <div class="card-body p-4">
-                            <div class="badge bg-primary bg-gradient rounded-pill mb-2">${item.category}</div>
+                            <div class="badge bg-secondary bg-gradient rounded-pill mb-2">${item.category}</div>
                             <a class="text-decoration-none link-dark stretched-link" href="#!">
                                 <h5 class="card-title mb-3">${item.title}</h5>
                             </a>
                             <p class="card-text mb-0">${item.description}</p>
                         </div>
                         <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                            <div class="d-flex align-items-center">
-                                <div class="small">
-                                    <!--div class="fw-bold">${item.author}</div-->
-                                    <div class="text-muted">${item.date}</div>
+                            <div class="d-flex align-items-end justify-content-between">
+                                <div class="d-flex align-items-center">
+                                    <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                    <div class="small">
+                                        <div class="fw-bold">${item.author}</div>
+                                        <div class="text-muted">${item.date}</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
